a6d98911f6d06637efc97694641ececbf317dfea
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-10-19 10:38:00 +0200

    without colors

---
+++ src/Button/Button.tsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+// #95a5a6 concrete
+// #e74c3c alizarin
+// #f39c12 orange
+// #2ecc71 emerald
+
+type ButtonProps = {
+  label: string
+  // bgColor: 
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button>Hello</button>
+  );
+}
+
+export default Button;
+// export { Button };
