f2c32b5eb3a0f52a51c3ca288633e67f6d3d0e81
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-10-20 15:52:42 +0200

    context without hooks

--- src/components/User/UserDetails.tsx
+++ src/components/User/UserDetails.tsx
@@ -1,5 +1,6 @@
-import { useUserContext } from 'components/UserContext';
+// import { useUserContext } from 'components/UserContext';
 import './User.css';
+import { UserConsumer } from 'components/UserContext';
 
 type UserDetailsProps = {
   email: string;
@@ -7,12 +8,17 @@ type UserDetailsProps = {
 }
 
 function UserDetails({ email, isLogged }: UserDetailsProps) {
-  const context = useUserContext();
+  // const context = useUserContext();
   return (
     <div className="ml">
       <h4>User Details</h4>
       <p>E-mail: {email}</p>
-      <p><strong>{context.isLogged ? 'Yes' : 'No'}</strong></p>
+      {/* <p><strong>{context.isLogged ? 'Yes' : 'No'}</strong></p> */}
+      <UserConsumer>
+        {(value) => (
+          <p><strong>{value.isLogged ? 'Yes' : 'No'}</strong></p>
+        )}
+      </UserConsumer>
     </div>
   );
 }
