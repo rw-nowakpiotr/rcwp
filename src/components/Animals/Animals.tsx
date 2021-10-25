35903b57f76fe3056fd1d628564df45df8ba6b09
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-10-25 12:01:26 +0200

    set animals in state

--- src/components/Animals/Animals.tsx
+++ src/components/Animals/Animals.tsx
@@ -1,6 +1,6 @@
 // import { Fragment } from 'react';
-
-import type { MouseEventHandler } from "react";
+// import type { MouseEventHandler } from "react";
+import { useState } from 'react';
 
 const data = [
   '🐕 dog',
@@ -10,14 +10,27 @@ const data = [
 ];
 
 function Animals() {
+  const [animals, setAnimals] = useState<string[]>(data);
   const handleRemove = (index: number) => {
     console.log('`My index is: ', index);
   }
+  // const handleRemove: MouseEventHandler<HTMLSpanElement> = (event) => {
+  //   console.log(event.currentTarget.dataset);
+  //   // console.log('`My index is: ', event.target.dataset.index);
+  // }
+  // const handleRemove = (index: number) => (event: MouseEvent) => {
+  //   console.log('`My index is: ', index);
+  // }
   return (
     <>
-      {data.map((elem, index) => {
+      {animals.map((elem, index) => {
         return (
-          <div>{elem} <span onClick={() => handleRemove(index)}>⛔</span></div>
+          <div>
+            {elem}{' '}
+            <span
+              data-index={index}
+              onClick={() => handleRemove(index)}>⛔</span>
+            </div>
         );
       })}
     </>
